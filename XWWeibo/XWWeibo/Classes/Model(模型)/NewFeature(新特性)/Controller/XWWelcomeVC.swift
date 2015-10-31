78e2a9f49799e2baf4f5c9ccc9d2a0c3f571b27b
sxm <sxm@qq.com>
2015-10-31 18:37:52 +0800

    初步做好登陆界面和封装FN

---
+++ XWWeibo/XWWeibo/Classes/Model(模型)/NewFeature(新特性)/Controller/XWWelcomeVC.swift
@@ -0,0 +1,13 @@
+//
+//  XWWelcomeVC.swift
+//  XWWeibo
+//
+//  Created by apple on 15/10/30.
+//  Copyright © 2015年 ZXW. All rights reserved.
+//
+
+import Cocoa
+
+class XWWelcomeVC: UIViewController {
+
+}
